fd5f36eae5778c6b3aa1daec92f6d849841122a2
Sharafutdinov Albert <chelninecz@gmail.com>
2024-02-10 09:48:10 +0300

    prettier fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default(array) => {
-    const length = array == null ? 0 : array.length;
-    return length ? array[length - 1] : undefined;
-  };
+export default (array) => {
+  const length = array == null ? 0 : array.length;
+  return length ? array[length - 1] : undefined;
+};
